16e65aee02477f364a09a30b7eff00a65cfd2d3f
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2020-11-24 21:20:07 -0300

    WIP:

--- app/main.py
+++ app/main.py
@@ -1,20 +1,23 @@
 from flask import Flask, request, jsonify
+from bot import command
+import logging
+
 app = Flask(__name__)
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
 @app.route('/')
 def hello_world():
-    thisdict = {
-        "brand": "Ford",
-        "model": "Mustang",
-        "year": 1964
-        }
+    command.hello()
     return 'Hello, from the slack bot with python and Flask 1.1.2!'
 
 @app.route('/api/v1/slack/outgoing-webhook', methods=["POST"])
-def form_xxx():
-    req = request.form
-    print(req)
-    return jsonify(req)
+def slackOutgoing():
+    outgoing = request.form
+    app.logger.info('Teste %s', outgoing['user_name'])
+    app.logger.info('Teste %s', outgoing['user_name'])
+    # print(outgoing)
+    return jsonify(outgoing)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0') # port=8080, debug=True
